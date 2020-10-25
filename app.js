e0f17a7e177a417893568407cd4a94882b9dd4ad
ThatcheRRR <pavelosipov777@mail.ru>
2020-10-26 02:45:22 +0300

    feat: add drawing line func

--- app.js
+++ app.js
@@ -13,17 +13,20 @@ fileElem.addEventListener('change', e => {
       switch(item[0]) {
         case 'C':
           createCanvas(item);
+          break;
         case 'L':
           drawLine(item);
+          break;
         case 'R':
           drawRect(item);
+          break;
         case 'B':
           bucketFill(item);
+          break;
         default:
           canDraw = false;
       }
     });
-    console.log(arr);
   };
 });
 
@@ -32,8 +35,8 @@ function createCanvas(canvas) {
     canDraw = false;
   } else {
     canDraw = true;
-    const w = +canvas[0][1];
-    const h = +canvas[0][2];
+    const w = +canvas[1];
+    const h = +canvas[2];
     for(let i = 0; i < h + 2; i++) {
       field.push(new Array(w + 2));
     }
@@ -50,7 +53,21 @@ function createCanvas(canvas) {
 };
 
 function drawLine(line) {
-
+  console.log(line)
+  const x1 = +line[1];
+  const y1 = +line[2];
+  const x2 = +line[3];
+  const y2 = +line[4];
+  if(x1 === x2) {
+    let i = y1;
+    while(i <= y2) {
+      field[i].splice(x1, 1, 'x');
+      i++;
+    }
+  } else {
+    const diff = x2 - x1;
+    field[y1].splice(x1, x2, ...'x'.repeat(diff + 1).split(''));
+  }
 };
 
 function drawRect(rect) {
